edc00ba4611aa5164192448ce1c2a970c9cd0acf
masaki-michida <stupid763@gmail.com>
2020-09-17 21:59:13 +0900

    日本語化

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'パスワードをリセットしました',
+    'sent' => 'パスワードのリセットリンクを送ります',
+    'throttled' => '暫しお待ちください',
+    'token' => 'リセットトークンが無効です.',
+    'user' => "メールアドレスに該当するユーザーが存在しません",
 
 ];
