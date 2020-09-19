9a3838a9af1ce8b4c67550af295e60d57bd268c8
masaki-michida <stupid763@gmail.com>
2020-09-19 16:25:46 +0900

    モーダルの上の閉じるボタン削除

--- resources/views/template/mordal.blade.php
+++ resources/views/template/mordal.blade.php
@@ -1,9 +1,6 @@
 <div class="modal fade" id="form-modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
       <div class="modal-body">
         @include('mypage/form')
       </div>
