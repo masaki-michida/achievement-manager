bf74f1014ae25f7d2b5285d7fbfc3732aeb7d5b9
masaki-michida <stupid763@gmail.com>
2020-09-19 15:05:08 +0900

    閉じるボタンを押した時にフォームを初期状態に戻すようにした

--- resources/views/template/mordal.blade.php
+++ resources/views/template/mordal.blade.php
@@ -8,9 +8,20 @@
         @include('mypage/form')
       </div>
       <div class="modal-footer form-group">
-             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-             <button type="button" class="btn btn-primary btn-submit btn-submit-new-post" data-dismiss="modal">Save changes</button>
+             <button type="button" class="btn btn-secondary modal-close" data-dismiss="modal">閉じる</button>
+             <button type="button" class="btn btn-primary btn-submit btn-submit-new-post">送信</button>
       </div>
     </div>
   </div>
 </div>
+
+<script>
+  $('.modal-close').on('click',(e)=>{
+    while(addInputCounter>0){
+      $(`.addedGoal${addInputCounter}`).remove();
+      addInputCounter -= 1;
+    }
+    addInputCounter = 1;
+      $('.form-control').val('');
+  })
+</script>
