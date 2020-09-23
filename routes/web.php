0483cd0395187fef731b3f896e8890c69742c576
masaki-michida <stupid763@gmail.com>
2020-09-23 12:38:10 +0900

    init

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('mypage','MypageController@index')->name('mypage');
 });
 
-Route::put('mypage/compTarget/{mypage}',['as'=>'mypage.ajaxCompTarget','uses'=>'MypageController@ajaxCompTarget']);
+Route::put('mypage/compTarget/{mypage}','MypageController@ajaxCompTarget')->name('mypage.ajaxCompTarget');
 Route::post('mypage','MypageController@ajaxRequestPost')->name('mypage.ajaxPost');
 Route::delete('mypage','MypageController@ajaxRequestDelete')->name('mypage.ajaxDelete');
-Route::put('mypage/checkBox',['as'=>'mypage.ajaxCheckBox','uses'=>'MypageController@ajaxCheckBox']);
+Route::put('mypage/checkBox','MypageController@ajaxCheckBox')->name('mypage.ajaxCheckBox');
